2019f80c8077533315a26af801b17b1e694edd05
GoReleaser <FelicianoTech@gmail.com>
2021-12-11 21:00:36 -0500

    Brew formula update for strawberry version v0.21.0

--- Formula/strawberry.rb
+++ Formula/strawberry.rb
@@ -5,22 +5,21 @@
 class Strawberry < Formula
   desc "An awesome static site generator based on Hugo."
   homepage "https://www.StrawberrySSG.com"
-  version "0.20.1"
-  bottle :unneeded
+  version "0.21.0"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/strawberryssg/strawberry-v0/releases/download/v0.20.1/strawberry-v0.20.1-macos-amd64.tar.gz"
-      sha256 "c7c4eaef41cb06923d31e514c71f03671bb1e5b6464e6f5ec1a15f074588b7fa"
+      url "https://github.com/strawberryssg/strawberry-v0/releases/download/v0.21.0/strawberry-v0.21.0-macos-amd64.tar.gz"
+      sha256 "95400ea8935a659333d716f9ce34a103b05f5a691afcc369f6f32b64b9bf3dad"
+
+      def install
+        bin.install "strawberry"
+      end
     end
   end
 
   depends_on "go"
-
-  def install
-    bin.install "strawberry"
-  end
 
   test do
     site = testpath/"hops-yeast-malt-water"
